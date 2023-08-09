45f093ecd5f4e96a93933dc8de03d8b075c3277a
Sam Alexander <61659157+EslamFouad1@users.noreply.github.com>
2023-08-09 16:27:47 +0300

    Update target_vs_features_graph.py

--- target_vs_features_graph.py
+++ target_vs_features_graph.py
@@ -7,3 +7,24 @@ for i in range(8):
     col = tr_d.iloc[:,i]
     sns.barplot(data=training_df, x='Class', y=col)
 plt.show()
+
+# Distribution Plot and Checking The Distribution for Normality
+plt.figure(figsize = (20, 13))
+plt.subplot(2,2,1)
+sns.histplot(data=training_df, x=training_df.loc[training_df['Class']==1,'Mean_Integrated'], kde=True)
+plt.title('Class 1')
+plt.subplot(2,2,2)
+st.probplot(training_df.loc[training_df['Class']==1, 'Mean_Integrated'], plot=plt)
+plt.title('Class 1')
+plt.subplot(2,2,3)
+sns.histplot(data=training_df, x=training_df.loc[training_df['Class']==0,'Mean_Integrated'], kde=True)
+plt.title('Class 0')
+plt.subplot(2,2,4)
+st.probplot(training_df.loc[training_df['Class']==0,'Mean_Integrated'], plot=plt)
+plt.title('Class 0')
+plt.suptitle('Distribution Plot and Checking the Distribution for Normality \n 
+ 
+ 
+')
+plt.tight_layout()
+plt.show()
