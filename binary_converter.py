1c193647a35ccf36af56377b3fd314d9e0329067
Sam Alexander <61659157+EslamFouad1@users.noreply.github.com>
2023-08-09 16:19:33 +0300

    Update binary_converter.py

--- binary_converter.py
+++ binary_converter.py
@@ -1,1 +1,6 @@
 df['lightduty'] = df['lightduty'].apply(lambda x: 0 if x=='No' else 1)
+
+#Convert the relevant variables to a Boolean type.
+df_1 = df.copy()   #Work with a new object.
+df_1.default.map(dict(yes = 1, no = 0)).astype(bool)
+df_1.default.value_counts()
