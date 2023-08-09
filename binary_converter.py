d2d39899f34f7f4e8e9eafb16dc719c2f35b1fdf
Sam Alexander <61659157+EslamFouad1@users.noreply.github.com>
2023-08-09 16:20:01 +0300

    Update binary_converter.py

--- binary_converter.py
+++ binary_converter.py
@@ -4,3 +4,11 @@ df['lightduty'] = df['lightduty'].apply(lambda x: 0 if x=='No' else 1)
 df_1 = df.copy()   #Work with a new object.
 df_1.default.map(dict(yes = 1, no = 0)).astype(bool)
 df_1.default.value_counts()
+
+# Do the same thing for other Boolean variables.
+bool_vars = ["housing", "loan", "term_deposit"]
+
+for var in bool_vars:
+    df_1[var] = df_1[var].map(dict(yes = 1, no = 0)).astype(bool)
+    
+    print(f"Converted {var} to Boolean.")
