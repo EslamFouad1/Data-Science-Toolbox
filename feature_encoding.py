64f6ef77ce4c4c680171b2582178d6790857df86
Sam Alexander <61659157+EslamFouad1@users.noreply.github.com>
2023-08-09 17:00:52 +0300

    Update feature_encoding.py

--- feature_encoding.py
+++ feature_encoding.py
@@ -6,3 +6,22 @@ color_dict     = {'J': 1, 'I': 2, 'H': 3, 'G': 4, 'F': 5, 'E': 6,'D': 7,}
 data['cut'] = data['cut'].map(cut_class_dict)
 data['clarity'] = data['clarity'].map(clarity_dict)
 data['color'] = data['color'].map(color_dict)
+
+
+# Another way
+diamonds_num = diamonds.loc[:,["carat","depth","table","x","y","z"]]
+diamonds_cat = diamonds.loc[:,["cut","color","clarity"]]
+
+from sklearn.preprocessing import OrdinalEncoder
+
+ordinal_categories = [
+  ["Fair","Good","Very Good","Premium","Ideal"],
+  ["J","I","H","G","F","E","D"],
+  ["I1","SI2","SI1","VS2","VS1","VVS2","VVS1","IF"]
+]
+
+ordinal_encoder = OrdinalEncoder(categories=ordinal_categories)
+diamonds_cat_encoded = ordinal_encoder.fit_transform(diamonds_cat)
+diamonds_cat_encoded[:3]
+
+ordinal_encoder.categories_
