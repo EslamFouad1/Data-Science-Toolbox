0f725612ed9fdc458c4e5c0d340db300660d78e1
Sam Alexander <61659157+EslamFouad1@users.noreply.github.com>
2023-08-09 16:34:36 +0300

    Update t-SNE.py

--- t-SNE.py
+++ t-SNE.py
@@ -9,3 +9,20 @@ plt.figure()
 sns.scatterplot(data=df_tsne, x="tsne1", y="tsne2", hue="diagnosis")
 plt.show()
 plt.close('all')
+
+import plotly.express as px
+fig = px.scatter(df_tsne, x="tsne1", y="tsne2", color="diagnosis"
+                 #size='petal_length', hover_data=['petal_width']
+                )
+fig.show()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+for diagnosis in df_tsne.diagnosis.unique():
+    ax.scatter(df_tsne.tsne1[df_tsne.diagnosis==diagnosis],df_tsne.tsne2[df_tsne.diagnosis==diagnosis],df_tsne.tsne3[df_tsne.diagnosis==diagnosis],label=diagnosis)
+ax.set_xlabel('tsne1')
+ax.set_ylabel('tsne2')
+ax.set_zlabel('tsne3')
+ax.legend(bbox_to_anchor=(1.05, 1), loc=2)
+plt.show()
+plt.close('all')
